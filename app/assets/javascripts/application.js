ee8a6f1b9771e5b520e6f6a067439ea20ec16d7b
Ruslan Palahin <MrRiddick7@gmail.com>
2013-02-11 14:59:10 +0200

    scss v1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+jQuery('.sub-menu').hide();
+jQuery('.sub-menu-admin').hide();
+
+jQuery(document).ready(function(){
+    jQuery('.user-menu').mouseover(function(){
+        jQuery('.sub-menu').show();
+        jQuery('.sub-menu-admin').show();
+        jQuery('.user-menu-username').css({color: '#382C23', background: "url('/images/user-menu-point-white.jpg') right top no-repeat" });
+jQuery('.user-info').css({background: '#F5F3F1', borderLeft: '1px solid #CCC0B3', borderTop: '1px solid #CCC0B3', borderRight: '1px solid #CCC0B3'});
+});
+jQuery('.user-menu').mouseout(function(){
+    jQuery('.sub-menu').hide();
+    jQuery('.sub-menu-admin').hide();
+    jQuery('.user-menu-username').css({color: '#fff', background: "url('/images/user-menu-point.jpg') right top no-repeat" });
+jQuery('.user-info').css({background: 'none', borderLeft: '1px solid #382C23', borderTop: '1px solid #382C23', borderRight: '1px solid #382C23'});
+});
+
+jQuery('.user-general-page .flash-notice').fadeOut(3000);
+});
